a45c2987331eebb2374957802f63e1215e2de694
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2016-09-01 13:44:38 -0400

    Initial documentation

---
+++ R/chipenrich.data_package_doc.R
@@ -0,0 +1,30 @@
+#' chipenrich.data: Data for chipenrich: gene set enrichment analysis for ChIP-seq data
+#'
+#' Supporting data for the chipenrich package. Includes pre-defined gene sets, gene locus definitions, and mappability estimates.
+#'
+#' @docType package
+#' @name chipenrich.data
+#'
+#' @import AnnotationDbi
+#' @import BiocGenerics
+#' @import methods
+#' @import GenomicRanges
+#' @importClassesFrom GenomeInfoDb Seqinfo
+#' @importFrom GenomeInfoDb seqinfo
+#' @import GO.db
+#' @importFrom IRanges IRanges
+#' @import KEGG.db
+#' @import org.Dm.eg.db
+#' @import org.Hs.eg.db
+#' @import org.Mm.eg.db
+#' @import org.Rn.eg.db
+#' @import reactome.db
+#' @import TxDb.Dmelanogaster.UCSC.dm3.ensGene
+#' @import TxDb.Dmelanogaster.UCSC.dm6.ensGene
+#' @import TxDb.Hsapiens.UCSC.hg19.knownGene
+#' @import TxDb.Hsapiens.UCSC.hg38.knownGene
+#' @import TxDb.Mmusculus.UCSC.mm9.knownGene
+#' @import TxDb.Mmusculus.UCSC.mm10.knownGene
+#' @import TxDb.Rnorvegicus.UCSC.rn6.refGene
+#' @importFrom S4Vectors queryHits subjectHits
+NULL
