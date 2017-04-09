01e5b18fe472b9f219225aa1c3ae5de4d8b021c6
Raymond Cavalcante <rcavalca@umich.edu>
2017-04-09 14:48:02 +0000

    Add zebrafish data and docs
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/chipenrich.data@4128 db2202da-8704-0410-b924-b125b3cd84ef

--- R/main.R
+++
@@ -1,6 +0,0 @@
-example_datasets = function() {
-  message("Example ChIP-seq peak datasets:");
-  print(grep("peaks_",data(package="chipenrich.data")$results[,3],value=T));
-
-  message("Use 'data(name of dataset)' to load it.");
-}
